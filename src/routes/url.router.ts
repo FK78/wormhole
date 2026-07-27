f3de19d5d056305cfffda84e237bc6474f3362a0
FK78 <34196381+FK78@users.noreply.github.com>
2026-07-27 17:42:54 +0100

    feat(url-management): add update endpoint and refactor data mapping

--- src/routes/url.router.ts
+++ src/routes/url.router.ts
@@ -1,10 +1,11 @@
 import { Router, type Request, type Response } from "express";
-import { createUrl, retrieveUrl } from "../controllers/url.controller.ts";
+import { createUrl, retrieveUrl, updateShortUrl } from "../controllers/url.controller.ts";
 import { validate } from "../middleware/validate.ts";
 
 const router = Router();
 
 router.post("/", validate(["url"]), createUrl);
+router.put("/:id", validate(["url"]), updateShortUrl);
 router.get("/:id", retrieveUrl);
 
 export default router;
